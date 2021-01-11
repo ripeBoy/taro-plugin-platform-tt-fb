92ada545ff9af04966326bf767a68904f79ca9d3
ripeBoy <liqiang@jsonmdedia.com>
2021-01-11 11:58:30 +0800

    fix: 升级依赖包

--- types/program.d.ts
+++ types/program.d.ts
@@ -2,7 +2,6 @@ import { TT } from '@tarojs/plugin-platform-tt';
 export default class FB extends TT {
     platform: string;
     runtimePath: string;
-    reactComponents: string;
     /**
      * 增加组件或修改组件属性
      */
